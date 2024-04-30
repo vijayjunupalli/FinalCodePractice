db7c63332ba581e65738929eb0ad08ba1234da52
vijay <vijay.junupalli@gmail.com>
2024-04-30 12:50:23 -0500

    second commit

--- src/main/java/com/example/demo/controller/FinalTestController.java
+++ src/main/java/com/example/demo/controller/FinalTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest/api")
 public class FinalTestController {
-
+//
     @GetMapping("/test")
     public String test(){
 
