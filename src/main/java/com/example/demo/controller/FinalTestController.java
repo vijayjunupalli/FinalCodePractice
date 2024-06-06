4b462dbcaad4cda120bc7e8310a278f118df55c9
vijay <vijay.junupalli@gmail.com>
2024-06-05 22:49:21 -0500

    Added centralized exception handler

--- src/main/java/com/example/demo/controller/FinalTestController.java
+++ src/main/java/com/example/demo/controller/FinalTestController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 
 import com.example.demo.data.Employee;
+import com.example.demo.exception.CentralException;
 import com.example.demo.exception.EmployeeDetailsEmptyException;
+import com.example.demo.exception.SecondException;
 import com.example.demo.service.EmpService;
 import com.example.demo.serviceImpl.EmpServiceImpl;
 import lombok.RequiredArgsConstructor;
@@ -23,18 +25,19 @@ public class FinalTestController {
 
     @PostMapping("/insert")
     @ResponseStatus(HttpStatus.CREATED)
-    public String insertEmpDetails(@RequestBody Employee employee){
+    public String insertEmpDetails(@RequestBody Employee employee) {
 
-        if(!ObjectUtils.isEmpty(employee))
-            throw new EmployeeDetailsEmptyException("Employee details are empty: Please provide the required details");
-        else
-            empService.saveEmpDetails(employee);
+        try {
+            if (employee.getName().equals("vijay")) {
+                throw new EmployeeDetailsEmptyException("Boolean true: First Name exception");
+            } else if (employee.getName().equals("junupalli")) {
+                throw new SecondException("Boolean false: Second Name  exception");
+            }
+        } catch (Exception ex) {
+            throw new CentralException(ex.getMessage());
+        }
+        return null;
 
-        return "Insertion success!!";
     }
 
-
-
-
-
-    }
+}
---
+++ src/main/java/com/example/demo/exception/CentralException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exception;
+
+public class CentralException extends RuntimeException {
+
+    public CentralException(String message){
+
+        super(message);
+
+
+    }
+}
